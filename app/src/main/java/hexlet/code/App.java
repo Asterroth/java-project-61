d0f0fa98a90e102351333d304b77aa6073a5298d
am <anton.malukhin@gmail.com>
2023-11-10 21:44:43 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,16 +13,29 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Cli.printMenu();
         switch (scanner.next()) {
-            case "1" -> Cli.greeter();
-            case "2" -> Even.genGame();
-            case "3" -> Calc.genGame();
-            case "4" -> Gcd.genGame();
-            case "5" -> Progression.genGame();
-            case "6" -> Prime.genGame();
-            case "0" -> System.exit(0);
-            default -> {
+            case "1":
+                Cli.greeter();
+                break;
+            case "2":
+                Even.genGame();
+                break;
+            case "3":
+                Calc.genGame();
+                break;
+            case "4":
+                Gcd.genGame();
+                break;
+            case "5":
+                Progression.genGame();
+                break;
+            case "6":
+                Prime.genGame();
+                break;
+            case "0":
+                break;
+            default:
                 System.out.println("Please enter number 1, 2, 3 ... 6 or 0 for exit.");
-            }
+                break;
         }
         scanner.close();
     }
